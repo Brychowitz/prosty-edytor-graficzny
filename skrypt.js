f9109c343f00fc4556159bddaaa5e30abde9430a
Przemysław Brych <przemek.brych123@gmail.com>
2019-11-07 22:45:44 +0100

    update
    dodanie zmiennej kolor i funkcji zmienKolor(event)

--- skrypt.js
+++ skrypt.js
@@ -13,7 +13,45 @@ function inicjuj(){
     }
     document.getElementById('plansza').innerHTML = html;
 
-document.addEventListener("keydown", function(event) {
-  console.log(event.key, event.which);
-})
+    document.addEventListener("keydown", zmienKolor);
+}
+
+var kolor = 'biały';
+
+function zmienKolor(event){
+    var key = event.which || event.keyCode;
+    console.log(key);
+    switch(key){
+        case 49:
+            kolor = 'biały';
+            break;
+        case 50:
+            kolor = 'czarny';
+            break;
+        case 51:
+            kolor = 'czerwony';
+            break;
+        case 52:
+            kolor = 'zielony';
+            break;
+        case 53:
+            kolor = 'niebieski';
+            break;
+        case 54:
+            kolor = 'żółty';
+            break;
+        case 55:
+            kolor = 'fuchsia';
+            break;
+        case 56:
+            kolor = 'pomarańczowy';
+            break;
+        case 57:
+            kolor = 'brązowy';
+            break;
+        case 48:
+            kolor = 'szary';
+            break;
+    }
+    document.getElementById('opis').innerHTML = "Kolor: " + kolor;
 }
